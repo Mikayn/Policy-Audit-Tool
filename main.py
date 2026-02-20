c504436bf09190ecc9c9796b057fac0d9ae4e4ec
Aryan Ghimire <ghimirearyan21@gmail.com>
2026-02-20 07:51:21 +0000

    Added z3 evaluation part

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ from rich import box                                                        # ty
 console = Console()
 POLICY_FILE = "policies.json"
 
+Z3_TYPE_MAP = {
+    "String": String,
+    "Bool": Bool,
+    "Int": Int
+}
+
 # =========================
 # LOAD / SAVE POLICIES
 # =========================
@@ -31,3 +37,63 @@ def save_policies(policies):
 
 policies = load_policies()
 policy_schemas = load_policy_schemas()
+
+# =========================
+# Z3 EVALUATION PARTS
+# =========================
+
+def sis_rule():
+    pass
+
+def exam_rule():
+    pass
+
+def lab_rule():
+    pass
+
+def privacy_rule():
+    pass
+
+RULE_MAP = {
+    "SIS": sis_rule,
+    "Exam": exam_rule,
+    "Lab": lab_rule,
+    "Privacy": privacy_rule,
+}
+
+def evaluate_policy(policy):
+    """
+    Evaluates a single policy rule and returns True if allowed, False if denied.
+    """
+    s = Solver()
+
+    schema = policy_schemas[policy["type"]]
+    z3_vars = {}
+
+    for name, type_name in schema["vars"].items():
+        constructor = Z3_TYPE_MAP[type_name]
+        z3_vars[name] = constructor(f"{name}_{policy['type']}")
+
+    # Bind values from policy to Z3 variables
+    for name, var in z3_vars.items():
+
+        value = policy.get(name)
+
+        if isinstance(var, SeqRef):  # String
+            s.add(var == StringVal(value))
+        else:
+            s.add(var == value)
+
+
+    # Apply rule
+    allowed = Bool(f"allowed_{policy['type']}")
+    rule_function = RULE_MAP[policy["type"]]
+
+    s.add(allowed == rule_function(z3_vars))
+
+    # Solve
+    if s.check() == sat:
+        model = s.model()
+        return bool(model[allowed])
+    return False
+
